24f0e32b27e4b2f208ca4bcb930b7a7c9894fc9a
jcausse <jcausse@itba.edu.ar>
2025-11-29 12:21:02 -0300

    Updates

--- examenes/2R_1C_2025/README.md
+++ examenes/2R_1C_2025/README.md
@@ -2,4 +2,4 @@
 
 * **Ejercicio 1**: OK
 * **Ejercicio 2**: Falta
-* **Ejercicio 3**: Falta
+* **Ejercicio 3**: OK
---
+++ examenes/2R_1C_2025/ej3/ej3.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+typedef struct node { 
+   const char * person; 
+   unsigned int popularity; 
+   struct node * tail; 
+} node; 
+ 
+typedef node * TList;
+
+TList upPop(TList l, const char * name, const unsigned int pop) {
+    // Caso base 1: Llegue al final de la lista y no encontre a la persona que buscaba
+    if (l == NULL) {
+        struct node * newNode = malloc(sizeof(struct node));
+        newNode->person = strdup(name);    // Hago este strdup por si en algun momento se probara pasando un char[] en lugar del string constante
+        newNode->popularity = pop;
+        newNode->tail = NULL;
+        return newNode;
+    }
+
+    // Caso base 2: Encuentro a la persona
+    if (strcmp(l->person, name) == 0) {
+        l->popularity += pop;
+        return l;
+    }
+
+    // Caso general:
+    TList ret = upPop(l->tail, name, pop);      // Avanzo en la lista hasta el final, y cuando vuelvo
+
+    // Caso 1: La popularidad del nodo actual es mayor o igual que la del nodo que me devolvieron.
+    //         En este caso NO intercambio los nodos.
+    if (l->popularity >= ret->popularity) {
+        l->tail = ret;
+        return l;    
+    }
+
+    // Caso 2: La popularidad del nodo actual es menor que la del nodo que me devolvieron. En este caso
+    //         intercambio el nodo actual con el que me devolvieron, y vuelvo a devolver el mismo.
+    l->tail = ret->tail;        // Mi nuevo nodo siguiente es el siguiente al devuelto
+    ret->tail = l;              // El siguiente del devuelto soy yo
+    return ret;                 // En las dos lineas de arriba me intercambie con el devuelto, y devuelvo siempre
+                                // el nodo con mas popularity
+}
+
+void freeList(TList l) {
+    if (l == NULL) {
+        return;
+    }
+    freeList(l->tail);
+    free((char *) l->person);
+    free(l);
+}
+
+int main(void) { 
+    TList myList = NULL;                // Lista Vacía 
+    myList = upPop(myList, "Cato", 5);  // {"Cato",5} 
+    myList = upPop(myList, "Paco", 3);  // {"Cato",5} -> {"Paco",3} 
+    myList = upPop(myList, "Mon" , 4);  // {"Cato",5} -> {"Mon" ,4} -> {"Paco",3} 
+    myList = upPop(myList, "Cato", 1);  // {"Cato",6} -> {"Mon" ,4} -> {"Paco",3} 
+    myList = upPop(myList, "Mon" , 2);  // {"Cato",6} -> {"Mon" ,6} -> {"Paco",3} 
+    myList = upPop(myList, "Paco", 2);  // {"Cato",6} -> {"Mon" ,6} -> {"Paco",5} 
+    myList = upPop(myList, "Paco", 2);  // {"Paco",7} -> {"Cato",6} -> {"Mon" ,6} 
+    myList = upPop(myList, "Mon" , 1);  // {"Paco",7} -> {"Mon" ,7} -> {"Cato",6} 
+    // Se agrega otro (PACO no es lo mismo que Paco) 
+    myList = upPop(myList, "PACO" , 7);   
+                            // {"Paco",7} -> {"Mon",7} -> {"PACO",7} -> {"Cato",6} 
+    myList = upPop(myList, "Cato" , 8);   
+                            // {"Cato",14} -> {"Paco",7} -> {"Mon",7} -> {"PACO",7} 
+    
+    freeList(myList);
+    puts("OK"); 
+    return 0; 
+}
